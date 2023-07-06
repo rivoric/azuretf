35474822fd3dedb08be5fd73fb2ea3fbce5b0ef4
Rich Lee <richard.lee@credera.co.uk>
2023-07-06 17:18:56 +0100

    fix: remove provider restraint as it is in terraform block above

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,9 +14,4 @@ terraform {
   }
 }
  
-provider "azurerm" {
-  # The "feature" block is required for AzureRM provider 2.x.
-  # If you're using version 1.x, the "features" block is not allowed.
-  version = "~>2.0"
-  features {}
-}
+provider "azurerm" {}
