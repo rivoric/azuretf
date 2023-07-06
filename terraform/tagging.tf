01d5c84f6137899fa4e8bc7704bb7bc111f356ba
Rich Lee <richard.lee@credera.co.uk>
2023-07-06 22:50:24 +0100

    fix: dont pass tagname, not used

--- terraform/tagging.tf
+++ terraform/tagging.tf
@@ -1,70 +1,71 @@
-locals {
-  environment_tags = [
-    "dev",
-    "test",
-    "qa",
-    "uat",
-    "staging",
-    "prod"
-  ]
-}
+# resource "azurerm_policy_definition" "enforce_resource_group_tags" {
+#   name         = "dicci_enforce_rg_tags"
+#   policy_type  = "Custom"
+#   mode         = "All"
+#   display_name = "Enforce Resource Group Tags"
 
-resource "azurerm_policy_definition" "enforce_resource_group_tags" {
-  name         = "dicci_enforce_rg_tags"
-  policy_type  = "Custom"
-  mode         = "All"
-  display_name = "Enforce Resource Group Tags"
+#   lifecycle {
+#     ignore_changes = [
+#       metadata
+#     ]
+#   }
 
-  lifecycle {
-    ignore_changes = [
-      metadata
-    ]
-  }
+#   parameters = <<PARAMETERS
+#     {
+#         "tagName": {
+#             "type": "String",
+#             "metadata": {
+#                 "displayName": "Tag Name",
+#                 "description": "Name of the tag, such as costCenter"
+#             }
+#         },
+#         "policy-effect": {
+#         "type": "String",
+#         "metadata": {
+#             "displayName": "Policy Effect",
+#             "description": "The available options for the Policy Effect"
+#         },
+#         "allowedValues": [
+#             "audit",
+#             "deny"
+#         ],
+#         "defaultValue": "audit"
+#         }
+#     }
+#     PARAMETERS
 
-  parameters = <<PARAMETERS
-    {
-        "tagName": {
-            "type": "String",
-            "metadata": {
-                "displayName": "Tag Name",
-                "description": "Name of the tag, such as costCenter"
-            }
-        },
-        "policy-effect": {
-        "type": "String",
-        "metadata": {
-            "displayName": "Policy Effect",
-            "description": "The available options for the Policy Effect"
-        },
-        "allowedValues": [
-            "audit",
-            "deny"
-        ],
-        "defaultValue": "audit"
-        }
-    }
-    PARAMETERS
-
-  policy_rule = <<POLICY_RULE
-    {
-        "if": {
-            "allOf": [
-                {
-                    "field": "type",
-                    "equals": "Microsoft.Resources/subscriptions/resourceGroups"
-                },
-                {
-                    "field": "[concat('tags[', parameters('tagName'), ']')]",
-                    "exists": "false"
-                }
-            ]
-        },
-        "then": {
-            "effect": "[parameters('policy-effect')]"
-        }
-    }
-    POLICY_RULE
-}
+#   policy_rule = <<POLICY_RULE
+#     {
+#         "if": {
+#             "allOf": [
+#                 {
+#                     "field": "type",
+#                     "equals": "Microsoft.Resources/subscriptions/resourceGroups"
+#                 },
+#                 {
+#                     "not": {
+#                         "field": "tags['environment']",
+#                         "in": [
+#                             "production",
+#                             "dev",
+#                             "test",
+#                             "staging",
+#                             "qa"
+#                         ]
+#                     }
+#                 },
+#                 {
+#                     "field": "[concat('tags[', parameters('tagName'), ']')]",
+#                     "exists": "false"
+#                 }
+#             ]
+#         },
+#         "then": {
+#             "effect": "[parameters('policy-effect')]"
+#         }
+#     }
+#     POLICY_RULE
+# }
 
 resource "azurerm_policy_definition" "enforce_resource_tags" {
   name         = "dicci_enforce_r_tags"
@@ -81,13 +82,6 @@ resource "azurerm_policy_definition" "enforce_resource_tags" {
 
   parameters = <<PARAMETERS
     {
-        "tagName": {
-            "type": "String",
-            "metadata": {
-                "displayName": "Tag Name",
-                "description": "Name of the tag, such as 'environment'"
-            }
-        },
         "policy-effect": {
             "type": "String",
             "metadata": {
@@ -153,9 +147,8 @@ resource "azurerm_policy_set_definition" "tagging_standards" {
 
   # Enforces the Environment tag on any resource group that is created.
   policy_definition_reference {
-    policy_definition_id = azurerm_policy_definition.enforce_resource_group_tags.id
+    policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/96670d01-0a4d-4649-9c89-2d3abc0a5025"
     parameter_values = jsonencode({
-      policy-effect = { value = "[parameters('policy-effect')]" },
       tagName       = { value = "Environment" }
     })
   }
@@ -165,7 +158,6 @@ resource "azurerm_policy_set_definition" "tagging_standards" {
     policy_definition_id = azurerm_policy_definition.enforce_resource_tags.id
     parameter_values = jsonencode({
       policy-effect = { value = "[parameters('policy-effect')]" },
-      tagName       = { value = "Environment" }
     })
   }
 
@@ -178,23 +170,23 @@ resource "azurerm_policy_set_definition" "tagging_standards" {
   # }
 }
 
-# resource "azurerm_subscription_policy_assignment" "dicci_tagging_standards" {
-#   name                 = "dicci_tagging_standards"
-#   policy_definition_id = azurerm_policy_set_definition.tagging_standards.id
-#   subscription_id      = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
-#   description          = ""
-#   display_name         = "Example Tagging Standards"
-#   location             = "North Europe"
+resource "azurerm_subscription_policy_assignment" "dicci_tagging_standards" {
+  name                 = "dicci_tagging_standards"
+  policy_definition_id = azurerm_policy_set_definition.tagging_standards.id
+  subscription_id      = "/subscriptions/${data.azurerm_client_config.current.subscription_id}"
+  description          = ""
+  display_name         = "Example Tagging Standards"
+  location             = "North Europe"
 
-#   parameters = <<PARAMETERS
-#         {
-#             "policy-effect": {
-#                 "value": "deny"
-#             }
-#         }
-#     PARAMETERS
+  parameters = <<PARAMETERS
+        {
+            "policy-effect": {
+                "value": "deny"
+            }
+        }
+    PARAMETERS
 
-#   identity {
-#     type = "SystemAssigned"
-#   }
-# }
+  identity {
+    type = "SystemAssigned"
+  }
+}
